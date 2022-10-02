220307c5c582f578c446d025a661e2d23e3e51d5
gesty <gestyxlinaga@gmail.com>
2022-10-01 19:44:43 -0700

    added 'about' page, added subdirectories for images/styles, and changed background image

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-console.log("hello hello")
+console.log("hello hello");
+
+/* About Section Scroll:
+  Using `IntersectionObserver` to check if section is in view aka being observed
+  If a section is intersecting the observer, add class 'show'
+  Otherwise it's 'hidden'
+*/
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 // hello - Gesty Linaga //
