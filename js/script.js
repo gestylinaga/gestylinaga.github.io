478bb8b363d6691f9e44e79136f1f63e72f4afd6
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2022-12-23 03:20:46 -0800

    removed ascii

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // hello - Gesty Linaga
 
-/* About Section Scroll Script:
+/*
   Using `IntersectionObserver` to check if section is in view aka being observed
   If a section is intersecting the observer, add class 'show'
   Otherwise it's 'hidden'
@@ -17,34 +17,14 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-
 // Random Ascii Signature
-const sig = [`
-                                 🏄
-  ╔═╗┌─┐┌─┐┌┬┐┬ ┬  ╦  ┬┌┐┌┌─┐┌─┐┌─┐
-  ║ ╦├┤ └─┐ │ └┬┘  ║  ││││├─┤│ ┬├─┤
-  ╚═╝└─┘└─┘ ┴  ┴   ╩═╝┴┘└┘┴ ┴└─┘┴ ┴
-  🤙
-  `,
+const sig = [
   `
                _       
    ___ ___ ___| |_ _ _ 
   | . | -_|_ -|  _| | |
   |_  |___|___|_| |_  |
   |___|         🏄|___|
-  `,
-  `
-                           888             
-                           888             
-                        🏄 888             
- .d88b.   .d88b.  .d8888b  888888 888  888 
-d88P"88b d8P  Y8b 88K      888    888  888 
-888  888 88888888 "Y8888b. 888    888  888 
-Y88b 888 Y8b.          X88 Y88b.  Y88b 888 
- "Y88888  "Y8888   88888P'  "Y888  "Y88888 
-     888                               888 
-Y8b d88P                          Y8b d88P 
- "Y88P"                            "Y88P"  
   `,
   `
                      __         
@@ -56,4 +36,4 @@ Y8b d88P                          Y8b d88P
                                 
   `,
 ]
-console.log(sig[Math.floor(Math.random() * sig.length)])
+console.log(sig[Math.floor(Math.random() * sig.length)]);
