fe1642da4860e7ad1b170a2acb72ef86361a4373
Gesty Linaga <83747778+gestylinaga@users.noreply.github.com>
2024-01-11 01:10:33 -0800

    fix: resize 'about' image and fix scroll functionality

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
 // gesty.dev 🤙
 console.log("cowabunga");
 
-const header = document.getElementById("header")
-const navList = document.querySelector("#nav-list")
-const navItems = navList.querySelectorAll("a")
-const articleList = document.querySelectorAll("article")
-const backLinks = document.querySelectorAll("#back")
+const header = document.querySelector("#header");
+const navList = document.querySelector("#nav-list");
+const navItems = navList.querySelectorAll("a");
+const articleList = document.querySelectorAll("article");
+const backLinks = document.querySelectorAll("#back");
 
 function getLinkTo(link) {
   const linksTo = link.getAttribute("href")
-  return linksTo.slice(1)
+  return linksTo.slice(1);
 }
 
+/* "active" class list not working properly,
+   problem might be in here somewhere.
+*/
 function showArticle(link) {
   const targetName = getLinkTo(link);
   const targetElement = document.getElementById(targetName);
-  header.classList.add("hidden")
+  header.classList.add("hidden");
+  header.classList.remove("active");
   articleList.forEach((article) => {
-    article.classList.add("hidden")
-  })
+    article.classList.remove("active");
+    article.classList.add("hidden");
+  });
   targetElement.classList.remove("hidden");
+  targetElement.classList.add("active");
 }
 
 function reset() {
   articleList.forEach((article) => {
-    article.classList.add("hidden")
+    article.classList.add("hidden");
+    article.classList.remove("active");
   })
-  header.classList.remove("hidden")
+  header.classList.remove("hidden");
+  header.classList.add("active");
 }
 
 function handleDirectLinks(locationHash) {
-  const current = locationHash.slice(1)
+  const current = locationHash.slice(1);
   for (article of articleList) {
-    const articleID = article.getAttribute("id")
-    const targetArticle = document.getElementById(articleID)
+    const articleID = article.getAttribute("id");
+    const targetArticle = document.getElementById(articleID);
     if (current === articleID) {
       reset();
-      header.classList.add("hidden")
-      targetArticle.classList.remove("hidden")
+      header.classList.add("hidden");
+      targetArticle.classList.remove("hidden");
     } else if (current === "") {
       reset();
     }
@@ -45,14 +53,14 @@ function handleDirectLinks(locationHash) {
 }
 
 navItems.forEach((link) => {
-  link.addEventListener("click", () => 
-    showArticle(link)
-  )
+  link.addEventListener("click", () => {
+    showArticle(link);
+  })
 })
 
 backLinks.forEach((link) => {
   link.addEventListener("click", () => {
-    reset()
+    reset();
   })
 })
 
